546d9112b08d3d3d2d0ef23e5d6cdcbe7821db7b
rzbar <138229965+rzbar@users.noreply.github.com>
2023-09-18 18:38:11 +0800

    Delete Assets directory

--- Assets/Scripts/NPC/Data/SceneRouteDataList_SO.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "SceneRouteDataList_SO", menuName = "Map/SceneRouteData")]
-public class SceneRouteDataList_SO : ScriptableObject
-{
-    public List<SceneRoute> sceneRouteList;
-}
--- Assets/Scripts/NPC/Data/ScheduleDataList_SO.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "ScheduleDataList_SO", menuName = "NPC Schedule/ScheduleDataList")]
-public class ScheduleDataList_SO : ScriptableObject
-{
-    public List<ScheduleDetails> scheduleList;
-}
