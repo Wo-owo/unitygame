638b2b06c543ac06978ea0784559821272ddbde2
rzbar <138229965+rzbar@users.noreply.github.com>
2023-09-18 09:47:37 +0800

    Revert "rzbar 第一次提交"

--- Assets/Scripts/Crop/Data/CropDatailList_SO.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "CropDatailList_SO", menuName = "Crop/CropDataList")]
-public class CropDatailList_SO : ScriptableObject
-{
-    public List<CropDetails> cropDatailList;
-}
--- Assets/Scripts/Inventory/Data SO/InventoryBag_SO.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName ="InventoryBag_SO",menuName = "Inventory/InventoryBag")]
-public class InventoryBag_SO : ScriptableObject
-{
-    public List<InventoryItem> itemList;
-}
